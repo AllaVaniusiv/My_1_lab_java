3dcd325f665a1709b4f4a5f153255bcbbf3ce36b
AllaVaniusiv <allavaniusiv@gmail.com>
2023-03-13 12:26:00 +0200

    adding my lab with fixed

--- src/ua/lviv/iot/algo/part1/lab1/Printer.java
+++ src/ua/lviv/iot/algo/part1/lab1/Printer.java
@@ -1,7 +1,8 @@
 package ua.lviv.iot.algo.part1.lab1;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @ToString
@@ -12,30 +13,11 @@ public class Printer {
     private boolean isDuplex;
     private int paperTrayCapacity;
     private int paperCount;
-    public Printer(){}
-    public Printer(String model, String type ,boolean isColor , boolean isDuplex,int paperTrayCapacity,int paperCount){
-        this.model=model;
-        this.type=type;
-        this.isColor=isColor;
-        this.isDuplex=isDuplex;
-        this.paperTrayCapacity=paperTrayCapacity;
-        this.paperCount=paperCount;
-        }
     private static Printer defaultPrinter = new Printer();
 
     public static Printer getInstance() {
         return defaultPrinter;
     }
-    public String ToString() {
-        return "Printer {"+
-            "model=" + model +
-                    ",type='" + type + '\'' +
-                    ",isColor='" + isColor + '\'' +
-                    ",isDuplex='" + isDuplex + '\'' +
-                    ",paperTrayCapacity='" + paperTrayCapacity + '\'' +
-                    ",paperCount=" + paperCount +
-                    '}';
-        }
 
 
     public void print(int pages) {
